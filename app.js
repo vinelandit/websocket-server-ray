7faf7c4a4c6f1d45fd2bd6d858b6fc9dd60a703d
Sam Healy <samhealy@yahoo.com>
2023-04-11 15:43:39 +0100

    working

--- app.js
+++ app.js
@@ -7,8 +7,53 @@ const app = express();
 app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "index.html")) });
 
+console.log('hello');
 
-var td = null; // will contain touch designer websocket client
+const commandList = {
+    fromClient: {
+        
+        movePaddle: [
+            'speed'
+        ],
+        submitInitials: [
+            'initials'
+        ],
+        clearClients: [
+        ]
+    },
+    fromMaster: {
+        grantGame: [
+            'playerID'
+        ],
+        livesLeft: [
+            'lives'
+        ]
+    }
+}
+
+
+const validate = function(data) {
+    var valid = true;
+    data = JSON.parse(data);
+    console.log('validating', data);
+    if(!data.to) valid = false;
+    const senderType = data.to == 'master' ? 'fromClient' : 'fromMaster';
+    if(!commandList[senderType][data.command]) valid = false;
+
+    if(senderType == 'fromClient' && data.to !== 'master') valid = false;
+    // if(senderType == 'fromMaster' && data.to == 'master') valid = false;
+
+    console.log('valid', valid);
+    return valid;
+   
+}
+
+var master = null; // will contain touch designer websocket client
+var clients = {};
+const times = {}; 
+var deadClients = {};
+
+const preroll = 5; // seconds before play
 
 const httpServer = http.createServer(app);
 const wss = new ws.Server({ server: httpServer });
@@ -18,18 +63,113 @@ wss.on("connection",
         console.log("Client connected");
         if(req.url.indexOf('TOUCHDESIGNER')>-1) {
             console.log('Registering Touchdesigner client');
-            td = ws;
+            master = ws;
         } else {
-            ws.onmessage =
-                (event) =>
-                {
-                    if(td!==null) {
-                        const msg = event.data;
-                        td.send(msg);
-                    }
-                    
+            const re = /\?pid\=(\d+)/;
+            const pidm = req.url.match(re);
+            if(pidm.length) {
+                
+           
+            
+                const pID = pidm[1];
+                var grantedPID = pID;
+
+                if(!clients['player' + pID]) {
+                    clients['player' + pID] = ws;
+                    times['player' + pID] = {
+                        'start': Date.now()
+                    };
+                } else {
+                    // slot is already occupied!
+                    grantedPID = -1;
                 }
+
+                ws.send(JSON.stringify({
+                    
+                    command: 'grantGame',
+                    payload: {
+                        playerID: grantedPID 
+                    },
+                    to: grantedPID,
+                    from: 'master'
+                }));
+
+                master.send(JSON.stringify({
+                    
+                    command: 'joinGame',
+                    payload: {
+                        playerID: grantedPID
+                    },
+                    to: 'master',
+                    from: 'server'
+                    
+                }));
+
+                if(grantedPID == -1) {
+                    // kill connection after sending denied message
+                    setTimeout(function(){
+                        ws.close();
+                    }, 1000);
+                }
+
+            } else {
+                // missing player ID
+                ws.close();
+            }
         }
+
+        ws.onmessage =
+            (event) =>
+            {
+                if(event.data != '2::') { // ignore keepalive ping
+                    
+                    if(validate(event.data)) {
+                        const data = JSON.parse(event.data);
+                        if(data.to == 'master') {
+                            const i = 'player'+data.from;
+
+                            if(deadClients[i] && data.command == 'submitInitials') {
+                                // add lifetime to data, submit initials, close connection
+                                data.payload.lifetime = Math.round(((times[i].end - times[i].start) / 1000) - preroll);
+                                event.data = JSON.stringify(data);
+                                deadClients[i].close();
+                                delete deadClients[i];
+                            }
+                            if(data.command == 'clearClients') {
+                                console.log('clearing clients');
+                                for(var j in clients) {
+                                    clients[j].close;
+                                    delete clients[j];
+                                }
+                                for(var j in deadClients) {
+                                    deadClients[j].close;
+                                    delete deadClients[j];
+                                }
+
+                            } else {
+                                master.send(event.data);
+                            }
+                        } else {
+                            const i = 'player'+data.to;
+                            if(clients[i]) {
+                                clients[i].send(event.data);
+                                if(data.command == 'livesLeft' && data.payload.lives <= 0) {
+                                    // game over, move client to dead clients list
+                                    deadClients[i] = clients[i];
+                                    times[i].end = Date.now();
+                                    delete clients[i];
+
+                                }
+                            }
+
+
+                        }
+
+                    }
+                }
+                
+                
+            }
         
     });
 
---
+++ public/js/index.js
@@ -0,0 +1,122 @@
+$(document).ready(function(){
+
+    
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const playerID = urlParams.get('pid');
+    console.log('playerID', playerID)
+
+    $('#submitInitials').click(function(){
+        const initials = ($('#i0').val() + $('#i1').val() + $('#i2').val()).trim();
+        if(initials !== '') {
+            send('submitInitials', { 'initials': initials });
+            location.href = '/thanks.html';
+        }
+    });
+
+    const send = function(command, payload) {
+        const obj = {
+            command: command,
+            payload: payload,
+            to: 'master',
+            from: playerID
+        }
+        console.log('sending', obj);
+        ws.send(JSON.stringify(obj));
+    }
+
+    const showPanel = function(panelID) {
+        $('.panel').not('#'+panelID).fadeOut(300);
+        $('.panel#'+panelID).fadeIn(300);
+    }
+
+    // const ws = new WebSocket("wss://websocket-server-ray.herokuapp.com/?pid="+playerID);
+    const ws = new WebSocket("ws://192.168.137.1:3000/?pid="+playerID);
+
+    var drawing = false;
+
+    ws.onopen =
+        () => {
+            console.log("Connected to the server");
+            /* window.onmousemove = (event) => {
+                if(drawing) {
+                    const x = event.clientX/window.innerWidth;
+                    const y = event.clientY/window.innerHeight;
+                    console.log(x + ", " + y)
+                    ws.send(JSON.stringify({x: x, y: y}));
+                }
+                
+            }; */
+
+            window.onmousedown = (event) => {
+                drawing = true;
+            }
+
+            window.onmouseup = (event) => {
+                drawing = false;
+            }
+
+            ws.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                if(data.command == 'livesLeft') {
+                    if(data.payload.lives <= 0) {
+                        // show game over 
+                        showPanel('gameOver');
+                    } else {
+                        $('#lives').html('0'+data.payload.lives);
+                    }
+                } else if (data.command == 'grantGame') {
+                    if(data.payload.playerID == playerID) {
+                        showPanel('play');
+                        // do start animation
+                    } else {
+                        showPanel('message');
+                    }
+                }
+                
+            }
+        };
+
+    $(window).resize(function(){
+        $('#slider-vertical').css({
+            height: $('#play').height()+'px'
+        });
+    });
+
+
+    $('#slider-vertical').css({
+        height: $('#play').height()+'px'
+    });
+
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "min",
+      min: -1,
+      max: 1,
+      value: 0,
+      step: 0.1,
+      slide: function( event, ui ) {
+        console.log(ui.value);
+
+        send('movePaddle', { speed: ui.value} );
+      },
+      stop: function(event, ui) {
+        
+        send('movePaddle', { speed: 0 });
+        $(ui.handle).animate({
+            'bottom': '50%'
+        }, 100, function(){
+        });
+
+      }
+    });
+
+    $('input').keyup(function(e){
+        console.log(e.originalEvent);
+        if(e.originalEvent.key !== 'Backspace' && e.originalEvent.key !== 'Tab' && e.originalEvent.key !== 'Shift') {
+            $(this).next().focus();
+        }
+    });
+
+    
+});
